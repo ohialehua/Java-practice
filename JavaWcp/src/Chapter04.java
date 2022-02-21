6cccd133649447d4e7fcca4b62146621de5028d9
Kairyu <kairyu.bonvivant@gmail.com>
2022-02-21 21:54:13 +0900

    [Add Chapter04]

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+public static void main(String[] args) {
+	final int LUCKY_NUMBER = 7;
+	System.out.println(LUCKY_NUMBER);
+	
+	String myname = "海江田 龍";
+	System.out.println(myname);
+	
+	myname = "Kairyu";
+	System.out.println(myname);
+}
+}

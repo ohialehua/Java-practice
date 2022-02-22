0990eecf1fd5a19b5ba81d08ef01c55b0fce105f
Kairyu <kairyu.bonvivant@gmail.com>
2022-02-22 20:28:10 +0900

    [Add Chapter05]

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,26 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 20000114L;
+		double double1 = 2000.0114;
+		boolean boolean1 = true;
+		Float float1 = 1.14f;
+		Integer integer1 = 114;
+		Character character1 = 'あ';
+
+		int[] array1 = new int[5];
+		array1[0] = 20;
+		array1[1] = 00;
+		array1[2] = 12;
+		array1[3] = 01;
+		array1[4] = 14;
+		int int1 = array1[3];
+		
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		System.out.println(float1);
+		System.out.println(integer1);
+		System.out.println(character1);
+		System.out.println(int1);
+	}
+}

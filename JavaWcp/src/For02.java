20f701b5a7945d0d6db441982f1d1c54bb736636
Kairyu <kairyu.bonvivant@gmail.com>
2022-02-23 19:07:04 +0900

    [Add Break01]

--- JavaWcp/src/For02.java
+++ JavaWcp/src/For02.java
@@ -2,7 +2,7 @@
 public class For02 {
 	public static void main(String[] args) {
 		int[] array = { 1, 2, 3, 4, 5 };
-		for (int number : array) {
+		for (int number : array) { //array配列の要素を1つずつnumberに代入
 			System.out.println("For02 = " + number);
 		}
 	}

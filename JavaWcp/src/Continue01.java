04180df582fa7bec8565ccef8a9eb301dbcbd745
Kairyu <kairyu.bonvivant@gmail.com>
2022-02-23 19:20:26 +0900

    [Add Recursive01]

--- JavaWcp/src/Continue01.java
+++ JavaWcp/src/Continue01.java
@@ -2,7 +2,7 @@
 public class Continue01 {
 	public static void main(String[] args) {
 		for (int count = 0; count < 5; count++) {
-			if (count == 2) {
+			if (count == 2) { //条件の時スキップ
 				continue;
 			}
 			System.out.println("Continue01 = " + count);

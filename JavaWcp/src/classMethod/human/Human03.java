1378801c0d5aa50007e7fc8747b313ce6516a252
Kairyu <kairyu.bonvivant@gmail.com>
2022-02-25 13:32:34 +0900

    [Add Human04]

--- JavaWcp/src/classMethod/human/Human03.java
+++ JavaWcp/src/classMethod/human/Human03.java
@@ -10,11 +10,11 @@ public class Human03 {
 		this.age = 20;
 		this.profession = "プログラマー";
 	}
-
+    //String型の戻り値を返す引数なしのメソッドを定義
 	public String getProfile() {
 		return "年齢は" + this.age + "、職業は" + this.profession;
 	}
-
+	//戻り値を返さない、String型の引数を1つとるメソッドを定義
 	public void greet(String friend) {
 		if (friend == null) {
 			System.out.println("挨拶する友達が分かりません！");

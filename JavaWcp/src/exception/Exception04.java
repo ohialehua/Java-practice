f3c4c6050e7288a1988a1c8c0edf1e55456603c8
Kairyu <kairyu.bonvivant@gmail.com>
2022-02-27 15:30:55 +0900

    [Add コメントアウト]

--- JavaWcp/src/exception/Exception04.java
+++ JavaWcp/src/exception/Exception04.java
@@ -11,12 +11,15 @@ public class Exception04 {
 		} catch (FileNotFoundException e) {
 			System.out.println("FileNotFoundException例外が発生");
 		}
+		// catch文で例外処理されるので、処理が実行される
 		System.out.println("プログラム修了");
 	}
 
 	public static void readFile(String fileName) throws FileNotFoundException {
+	//修飾子 戻り値の型 メソッド名(引数)         throws 例外クラスの型(カンマ（,）区切りで複数指定可)
 		System.out.println("ファイルの読み込み開始");
-		FileReader fr = new FileReader(fileName);
+		FileReader fr = new FileReader(fileName); //ここまで実行される
+		         //今回は使わない変数
 		System.out.println(fileName + "の読み込み完了");
 	}
 }

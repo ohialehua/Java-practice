2c04190ac1e31448f56db97595fc4801ec4912dc
Kairyu <kairyu.bonvivant@gmail.com>
2022-02-27 15:28:24 +0900

    [Exception04]

--- JavaWcp/src/exception/Exception03.java
+++ JavaWcp/src/exception/Exception03.java
@@ -5,18 +5,21 @@ public class Exception03 {
 	public static void main(String[] args) {
 		try {
 			Exception03.division(100, 0);
-		} catch (ArithmeticException e) {
+		} catch (ArithmeticException e) { //算術計算でのエラー
 			System.out.println("ArithmeticException例外が発生");
-		} catch (IllegalArgumentException e) {
+		} catch (IllegalArgumentException e) { //メソッドに不正な引数を渡すことによるエラー	
 			System.out.println("IllegalArgumentException例外が発生");
-			throw e;
+			// 例外インスタンスをスロー
+			throw e; //ここまで実行される
 		}
+		// finallyブロックでないので処理が実行されない
 		System.out.println("プログラム修了");
 	}
 
 	public static void division(int a, int b) {
 		System.out.println(a + " ÷ " + b + "は？");
 		if (b == 0) {
+			// 例外インスタンスを生成してスロー
 			throw new IllegalArgumentException("引数が不正です");
 		}
 		int result = a / b;

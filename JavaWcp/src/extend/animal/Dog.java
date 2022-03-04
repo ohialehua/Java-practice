7bd310c9c0b0887f2b56a5520df589207a70db66
Kairyu <kairyu.bonvivant@gmail.com>
2022-03-04 09:18:20 +0900

    [Add Dog]

---
+++ JavaWcp/src/extend/animal/Dog.java
@@ -0,0 +1,12 @@
+package extend.animal;
+
+public class Dog {
+
+	 public Dog() {
+	        this.name = "犬";
+	    }
+
+	    public void cry() {
+	        System.out.println(this.name + "が吠えました。ワン！ワン！");
+	    }
+}

1b597822668f1f01e0fa2d790c2343a46e13649e
Kairyu <kairyu.bonvivant@gmail.com>
2022-03-08 10:09:59 +0900

    [Fix Phone]

--- JavaWcp/src/extend/phone/Phone.java
+++ JavaWcp/src/extend/phone/Phone.java
@@ -5,4 +5,8 @@ public interface Phone {
     int MAX_NUMBER_DIGITS = 11;
  // public abstractが自動で追加される（抽象メソッド）
     public void call(String number);
+ // defaultメソッドの宣言
+    default void powerOff() {
+        System.out.println("電源を切ります。");
+    }
 }

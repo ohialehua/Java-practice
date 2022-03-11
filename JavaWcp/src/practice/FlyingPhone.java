13418d7bb7b2ca70ebef8ddda8ba740229f9d9c2
Kairyu <kairyu.bonvivant@gmail.com>
2022-03-11 19:21:42 +0900

    [Fix FlyingPhone]

--- JavaWcp/src/practice/FlyingPhone.java
+++ JavaWcp/src/practice/FlyingPhone.java
@@ -3,4 +3,21 @@ package practice;
 import extend.phone.Phone;
 
 public class FlyingPhone implements Phone, Flying {
+	private int minute;
+	
+	public FlyingPhone(int minute) {
+		this.minute = minute;
+	}
+	
+	public void fly() {
+		System.out.println(this.minute + "分間、飛びます。");
+	}
+	
+	public void call(String Number) {
+		System.out.println(Number + "に電話します。通話できるのは飛んでいる間だけです。");
+	}
+	
+	public void powerOff() {
+		Flying.super.powerOff();
+	}
 }
